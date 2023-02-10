e0af415d76a7fa91ab1906d832598e759d057032
Kevin Lang <kevinl20@yahoo.com>
2023-02-09 16:31:04 -0800

    Added Dependencies and uuid to grab notes,add new notes, and update notes

---
+++ db/Datasave.js
@@ -0,0 +1,54 @@
+// Dependencies
+const util = require('util');
+const fs = require('fs');
+const uuidV1 = require('uuid/v1');
+
+
+const readNote = util.promisify(fs.readFile);
+const writeNote = util.promisify(fs.writeFile);
+
+class Save {
+    write(note) {
+        return writeNote('db/db.json', JSON.stringify(note));
+    }
+
+    read() {
+        return readNote('db/db.json', 'utf8');
+    }
+
+    retrieveNotes() {
+        return this.read().then(notes => {
+            let parsedNotes;
+            try {
+                parsedNotes = [].concat(JSON.parse(notes));
+            } catch (err) {
+                parsedNotes = [];
+            }
+            return parsedNotes;
+        });
+    }
+
+    addNote(note) {
+        const { title, text } = note;
+        if (!title || !text) {
+            throw new Error('Both title and text can not be blank');
+        }
+
+        const newNote = { title, text, id: uuidV1() };
+
+        // grab Notes, add the new note, update notes
+        return this.retrieveNotes()
+            .then(notes => [...notes, newNote])
+            .then(updatedNotes => this.write(updatedNotes))
+            .then(() => newNote);
+    }
+
+    // BONUS: Delete Note 
+    deleteNote(id) {
+        return this.retrieveNotes()
+            .then(notes => notes.filter(note => note.id !== id))
+            .then(filteredNotes => this.write(filteredNotes));
+    }
+}
+
+module.exports = new Save();
